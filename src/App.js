791ebd5f67999ee47347893910bb19c25891ca6e
giacomomaggiore <giaco.maggiore@gmail.com>
2021-01-10 11:59:34 +0100

    added the whole netflix-clone project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Row from "./Row"
+import requests from "./request"
+import Banner from "./Banner"
+import Nav from "./Nav"
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Banner />
+
+
+      <Row
+        isLargeRow
+        title="NETLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horrors" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
